d63afd0b07c38f5125009f7a9d28d8b37efd49a7
Chase Moskal <chasemoskal@gmail.com>
2020-03-25 16:02:40 -0700

    update: authoritarian and redcrypto

--- source/main.ts
+++ source/main.ts
@@ -3,11 +3,12 @@ import Koa from "koa"
 import mount from "koa-mount"
 import {apiServer} from "renraku/dist/api-server.js"
 
+import {curryVerifyToken} from "redcrypto/dist/curries/curry-verify-token.js"
+
 import {read, readYaml} from "authoritarian/dist/toolbox/reading.js"
 import {connectMongo} from "authoritarian/dist/toolbox/connect-mongo.js"
 import {ProfileApi, ProfileServerConfig} from "authoritarian/dist/interfaces.js"
 import {unpackCorsConfig} from "authoritarian/dist/toolbox/unpack-cors-config.js"
-import {makeVerifyToken} from "authoritarian/dist/toolbox/tokens/make-verify-token.js"
 import {makeProfileMagistrate} from "authoritarian/dist/business/profile-magistrate/magistrate.js"
 import {mongoProfileDatalayer} from "authoritarian/dist/business/profile-magistrate/mongo-profile-datalayer.js"
 
@@ -23,7 +24,7 @@ const paths = {
 	const collection = await connectMongo(config.mongo, "profiles")
 
 	const profileMagistrate = makeProfileMagistrate({
-		verifyToken: makeVerifyToken(authServerPublicKey),
+		verifyToken: curryVerifyToken(authServerPublicKey),
 		profileDatalayer: mongoProfileDatalayer({collection}),
 	})
 
